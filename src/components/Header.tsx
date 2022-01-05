44bfe46cf513cb01f48aa98a75ffc3689a71ff10
Leebonggi <leebonggu12@hanmail.net>
2022-01-05 19:00:52 +0900

    reset

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,15 +11,6 @@ import {
   AuthItem,
 } from './Header.style';
 
-<<<<<<< HEAD
-console.log(4)
-console.log(5)
-=======
-console.log(1)
-console.log(2)
-console.log(3)
->>>>>>> main
-
 function Header() {
   return (
     <HeaderContainer>
