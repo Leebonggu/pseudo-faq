761d0a9308cb7e65d519150d2c063aa5bedd846e
Leebonggi <leebonggu12@hanmail.net>
2022-01-05 23:54:25 +0900

    add helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { initializeFaqData } from "@modules/faq";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { Helmet } from "react-helmet";
+import { initializeFaqData } from "@modules/faq";
 import { FAQPage } from '@pages/index';
 import Header from "@components/Header";
 
@@ -11,6 +12,10 @@ function App() {
   }, [dispatch])
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>MOIN PSEUDO FAQ</title>
+      </Helmet>
       <Header />
       <FAQPage />
     </>
